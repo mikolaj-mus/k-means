7c8acefe7be71930dd1893104f3e896c0cea9f67
mikolaj-mus <mus_mikolaj@wp.pl>
2025-04-03 12:44:06 +0200

    Refactor code and displayed plot

--- main.py
+++ main.py
@@ -1,83 +1,93 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+plt.ion()
+fig, ax = plt.subplots()
 
 cov = [[0.01, 0], [0, 0.01]]
 
 mean = [np.random.random(), np.random.random()]
 x1, y1 = np.random.multivariate_normal(mean, cov, 100).T
-plt.scatter(x1, y1, color='black', marker='x')
 
 mean = [np.random.random(), np.random.random()]
 x2, y2 = np.random.multivariate_normal(mean, cov, 100).T
-plt.scatter(x2, y2,  color='black', marker='x')
 
 mean = [np.random.random(), np.random.random()]
 x3, y3 = np.random.multivariate_normal(mean, cov, 100).T
-plt.scatter(x3, y3,  color='black', marker='x')
-plt.xlim(-0.1, 1.1)
-plt.ylim(-0.1, 1.1)
+
 S_all = list(zip(np.concatenate([x1, x2, x3]), np.concatenate([y1, y2, y3])))
-print(S_all)
 
 m1 = (np.random.random(), np.random.random())
 m2 = (np.random.random(), np.random.random())
 m3 = (np.random.random(), np.random.random())
 
-plt.plot(m1[0], m1[1], color='red', marker='o', markersize=25)
-plt.plot(m2[0], m2[1], color='green', marker='*', markersize=25)
-plt.plot(m3[0], m3[1], color='blue', marker='<', markersize=25)
-print(m1)
-print(m2)
-print(m3)
-S1 = []
-S2 = []
-S3 = []
-S1_old = []
-S2_old = []
-S3_old = []
-plt.show()
+ax.scatter(x1, y1, color='black', marker='x')
+ax.scatter(x2, y2, color='black', marker='x')
+ax.scatter(x3, y3, color='black', marker='x')
+
+centroid1, = ax.plot(m1[0], m1[1], 'ro', markersize=20)
+centroid2, = ax.plot(m2[0], m2[1], 'g*', markersize=20)
+centroid3, = ax.plot(m3[0], m3[1], 'b<', markersize=20)
+
+ax.set_xlim(-0.1, 1.1)
+ax.set_ylim(-0.1, 1.1)
+
+ax.text(0, 1.15, "Iteration: 0", fontsize=20, color='black', ha='center')
+
+plt.pause(1)
+
+# K-means iteration
+S1_old = S2_old = S3_old = []
+iteration = 0
 
 while True:
+    iteration += 1
+    S1, S2, S3 = [], [], []
+
     for point in S_all:
-        m1_edist = np.sqrt(np.power(m1[0] - point[0], 2) + np.power(m1[1] - point[1], 2))
-        m2_edist = np.sqrt(np.power(m2[0] - point[0], 2) + np.power(m2[1] - point[1], 2))
-        m3_edist = np.sqrt(np.power(m3[0] - point[0], 2) + np.power(m3[1] - point[1], 2))
-        smallest_dist = None
-        if m1_edist < m2_edist:
-            if m1_edist < m3_edist:
-                S1.append(point)
-            else:
-                S3.append(point)
-        elif m2_edist < m3_edist:
+        d1 = np.linalg.norm(np.array(point) - np.array(m1))
+        d2 = np.linalg.norm(np.array(point) - np.array(m2))
+        d3 = np.linalg.norm(np.array(point) - np.array(m3))
+
+        if d1 < d2 and d1 < d3:
+            S1.append(point)
+        elif d2 < d3:
             S2.append(point)
         else:
             S3.append(point)
 
     if S1 == S1_old and S2 == S2_old and S3 == S3_old:
+        plt.close()
         break
-    if S1.__len__() != 0:
-        x,y = zip(*S1)
-        plt.scatter(x, y, color='red', marker='o')
-        m1 = (np.mean(x), np.mean(y))
-    if S2.__len__() != 0:
-        x,y = zip(*S2)
-        plt.scatter(x, y, color='green', marker='*')
-        m2 = (np.mean(x), np.mean(y))
-    if S3.__len__() != 0:
-        x,y = zip(*S3)
-        plt.scatter(x, y, color='blue', marker='<')
-        m3 = (np.mean(x), np.mean(y))
-    plt.plot(m1[0], m1[1], color='red', marker='o', markersize=25)
-    plt.plot(m2[0], m2[1], color='green', marker='*', markersize=25)
-    plt.plot(m3[0], m3[1], color='blue', marker='<', markersize=25)
-    plt.xlim(-0.1, 1.1)
-    plt.ylim(-0.1, 1.1)
-    plt.show()
 
-    S1_old = S1.copy()
-    S2_old = S2.copy()
-    S3_old = S3.copy()
-    S1.clear()
-    S2.clear()
-    S3.clear()
+    ax.clear()
+    ax.set_xlim(-0.1, 1.1)
+    ax.set_ylim(-0.1, 1.1)
+
+    if S1:
+        x, y = zip(*S1)
+        ax.scatter(x, y, color='red', marker='o')
+        m1 = (np.mean(x), np.mean(y))
+
+    if S2:
+        x, y = zip(*S2)
+        ax.scatter(x, y, color='green', marker='*')
+        m2 = (np.mean(x), np.mean(y))
+
+    if S3:
+        x, y = zip(*S3)
+        ax.scatter(x, y, color='blue', marker='<')
+        m3 = (np.mean(x), np.mean(y))
+
+    ax.plot(m1[0], m1[1], 'ro', markersize=20)
+    ax.plot(m2[0], m2[1], 'g*', markersize=20)
+    ax.plot(m3[0], m3[1], 'b<', markersize=20)
+
+    ax.text(0.08, 1.15, f"Iteration: {iteration}", fontsize=20, color='black', ha='center')
+
+    plt.pause(1)
+
+    S1_old, S2_old, S3_old = S1[:], S2[:], S3[:]
+
+plt.ioff()
+plt.show()
